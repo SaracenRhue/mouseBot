167da9afbeb2bf661846dfae76e233bd2bcde533
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2021-01-06 17:24:22 +0100

    Add files via upload

---
+++ MouseBot.py
@@ -0,0 +1,10 @@
+
+
+# left click
+mouse.click('left')
+
+# right click
+mouse.click('right')
+
+# drag from (0, 0) to (100, 100) relatively with a duration of 0.1s
+mouse.drag(0, 0, 100, 100, absolute=False, duration=0.1)
